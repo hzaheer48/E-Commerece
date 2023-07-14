f6e03a3dd386121943d33d8d2fa3e77ff94bb955
hzaheer48 <hzaheer48@outlook.com>
2023-07-14 23:17:18 +0500

    done

--- frontend/src/components/helper/Card1Component.js
+++ frontend/src/components/helper/Card1Component.js
@@ -22,7 +22,7 @@ function Card1Component({ width }) {
       <Card.Body>
         <Card.Title>Card Title</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
+          Some quick example text to build on the card title and make up the the
           bulk of the card's content.
         </Card.Text>
         <button
